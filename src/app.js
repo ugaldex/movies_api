ad00a137c4bddcac2a32409145a54ca7cba8d453
Gabriel Ugalde de Lima <gabrlima@motorola.com>
2025-06-01 16:15:40 -0400

    Adicionando CORS

--- src/app.js
+++ src/app.js
@@ -1,14 +1,19 @@
 const express = require('express');
 const cors = require('cors');
 const app = express();
-const movieRoutes = require('./routes/movieRoutes');
-require('dotenv').config();
-
 app.use(cors({
-    origin: ['capacitor://localhost', 'http://localhost', 'http://localhost:8100'],
+    origin: [
+        'capacitor://localhost',
+        'http://localhost',
+        'http://localhost:8100',
+        'https://localhost'
+    ],
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
     allowedHeaders: ['Content-Type', 'Authorization'],
 }));
+const movieRoutes = require('./routes/movieRoutes');
+require('dotenv').config();
+
 app.use(express.json());
 app.use('/movies', movieRoutes);
 
