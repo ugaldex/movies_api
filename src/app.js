098d84082d311dfbdc8696d6d06057e9768d84f8
Gabriel Ugalde de Lima <gabrlima@motorola.com>
2025-06-01 16:10:26 -0400

    Adicionando CORS

--- src/app.js
+++ src/app.js
@@ -5,9 +5,9 @@ const movieRoutes = require('./routes/movieRoutes');
 require('dotenv').config();
 
 app.use(cors({
-    origin: 'http://localhost:8100', // permite o frontend Ionic
-    methods: ['GET', 'POST', 'PUT', 'DELETE'],
-    credentials: true // se você estiver usando cookies/autenticação
+    origin: ['capacitor://localhost', 'http://localhost', 'http://localhost:8100'],
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
 }));
 app.use(express.json());
 app.use('/movies', movieRoutes);
